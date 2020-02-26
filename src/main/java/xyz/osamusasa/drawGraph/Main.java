6a3a9faf0b3e425918c6d0d606e67aa718e5a975
sasanoosamu <osamusasa@osamusasa.xyz>
2020-02-26 15:25:39 +0900

    add function of draw graph

---
+++ src/main/java/xyz/osamusasa/drawGraph/Main.java
@@ -0,0 +1,53 @@
+package xyz.osamusasa.drawGraph;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String args[]){
+        JFrame frame = new JFrame("draw graph");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //from https://nat.hatenadiary.com/entry/20050513/p1
+        //--
+        java.awt.GraphicsEnvironment env = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
+        // 変数desktopBoundsにデスクトップ領域を表すRectangleが代入される
+        java.awt.Rectangle desktopBounds = env.getMaximumWindowBounds();
+        //--
+        //windowサイズを画面の最大サイズに設定
+        frame.setBounds(desktopBounds);
+
+        int nodeRedius = 5;
+        Canvas canvas = new Canvas(){
+            @Override
+            public void paint(Graphics g) {
+                UndirectedGraph graph = UndirectedGraph.getTestData();
+
+                Arrays.asList(graph.edges).forEach(e->{
+                    g.setColor(Color.BLACK);
+                    g.drawLine((int)e.n1.pos.getX(), (int)e.n1.pos.getY(), (int)e.n2.pos.getX(), (int)e.n2.pos.getY());
+                });
+
+                Arrays.asList(graph.nodes).forEach(e->{
+                    g.setColor(Color.BLACK);
+                    g.drawOval(
+                            (int)e.pos.getX()-nodeRedius,
+                            (int)e.pos.getY()-nodeRedius,
+                            2*nodeRedius,
+                            2*nodeRedius);
+                    g.setColor(Color.WHITE);
+                    g.fillOval(
+                            (int)e.pos.getX()-nodeRedius,
+                            (int)e.pos.getY()-nodeRedius,
+                            2*nodeRedius,
+                            2*nodeRedius);
+                });
+            }
+        };
+
+        frame.getContentPane().add(canvas);
+        canvas.repaint();
+        frame.setVisible(true);
+    }
+}
---
+++ src/main/java/xyz/osamusasa/drawGraph/UndirectedGraph.java
@@ -0,0 +1,44 @@
+package xyz.osamusasa.drawGraph;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+public class UndirectedGraph {
+    Node[] nodes;
+    Edge[] edges;
+
+    static UndirectedGraph getTestData(){
+        UndirectedGraph g = new UndirectedGraph();
+        g.nodes = new Node[4];
+        g.nodes[0] = g.new Node("A", new Point( 50, 50));
+        g.nodes[1] = g.new Node("B", new Point(150, 50));
+        g.nodes[2] = g.new Node("C", new Point( 50,150));
+        g.nodes[3] = g.new Node("D", new Point(150,150));
+
+        g.edges = new Edge[4];
+        g.edges[0] = g.new Edge(g.nodes[0], g.nodes[1]);
+        g.edges[1] = g.new Edge(g.nodes[1], g.nodes[2]);
+        g.edges[2] = g.new Edge(g.nodes[2], g.nodes[3]);
+        g.edges[3] = g.new Edge(g.nodes[3], g.nodes[0]);
+
+        return g;
+    }
+
+    class Node{
+        String name;
+        Point2D pos;
+
+        public Node(String name, Point2D pos) {
+            this.name = name;
+            this.pos = pos;
+        }
+    }
+    class Edge{
+        Node n1, n2;
+
+        public Edge(Node n1, Node n2) {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+    }
+}
